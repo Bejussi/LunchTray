21dc1f264d59ca1857f827fa1c24badaf175b054
Alexandra <pindooqy@gmail.com>
2022-07-08 12:46:40 +0300

    Create model

---
+++ app/src/main/java/com/bejussi/lunchtray/model/MenuItem.kt
@@ -0,0 +1,12 @@
+package com.bejussi.lunchtray.model
+
+import java.text.NumberFormat
+
+data class MenuItem(
+    val name: String,
+    val description: String,
+    val price: Double,
+    val type: Int
+) {
+    fun getFormattedPrice(): String = NumberFormat.getCurrencyInstance().format(price)
+}
